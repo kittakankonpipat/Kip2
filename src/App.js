1e7554241d1efffc2f44360e90d54de327977672
kittaknkonpipat <kittakan77@gmail.com>
2025-09-26 12:26:19 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+    return(
+        <div>
+            <h1>Create ReactJS Manually</h1>
+            <p>Occaecat duis exereitation proident ullamco eu.</p>
+        </div>
+    );
+}
+
+export default App;
